273647f2d66138d454e38c75487246e722fdf64d
tylern4 <tylern@canisius.edu>
2022-11-11 09:38:13 -0500

    Update hadd location

--- hipo2root.sh
+++ hipo2root.sh
@@ -3,9 +3,11 @@
 source /group/clas12/packages/setup.sh
 module purge
 module load gcc/9.2.0
-module load root/6.24.06
+
+source /work/clas12/users/tylern/software/root/bin/thisroot.sh
 
 export PATH=/work/clas12/users/tylern/software/hipo_tools/bin:$PATH
+
 
 
 echo "======= dst2root ========="
@@ -13,6 +15,7 @@ echo "======= dst2root ========="
 echo "======= dst2root ========="
 
 echo "======= hadd ========="
-/site/12gev_phys/2.5/Linux_CentOS7.7.1908-gcc9.2.0/root/6.24.06/bin/hadd merged.root *.root
+mkdir tmp
+/work/clas12/users/tylern/software/root/bin/hadd -j16 -d $PWD/tmp merged.root *.root
 echo "======= hadd ========="
 
